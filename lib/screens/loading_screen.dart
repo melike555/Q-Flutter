e354ac6d393ee8a885c4c5a75364b20319e3b3b4
melikeersoy <ersoym849@gmail.com>
2025-01-23 19:46:14 +0300

    loading_screen.dart guncelledi assets guncellendi

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,134 +1,56 @@
-// ignore_for_file: prefer_const_constructors
-
+// acilis ekranimiz buraya gelicek
+import 'package:dotlottie_loader/dotlottie_loader.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController epostaYonetici = TextEditingController();
-  TextEditingController sifreYonetici = TextEditingController();
-
-  girisYap() {
-    if (epostaYonetici.text.isEmpty || sifreYonetici.text.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Bilgileriniz Giriniz"),
-          // action: SnackBarAction(label: "Kapat", onPressed: () {}),
-          // margin: EdgeInsets.all(20),
-          behavior: SnackBarBehavior.floating,
-          backgroundColor: Colors.red,
-          showCloseIcon: true,
-        ),
-      );
-    } else {
-      if (sifreYonetici.text.length < 8) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text("Sifre Minimum 8 haneli olabilir."),
-            // action: SnackBarAction(label: "Kapat", onPressed: () {}),
-            // margin: EdgeInsets.all(20),
-            behavior: SnackBarBehavior.floating,
-            backgroundColor: Colors.red,
-            showCloseIcon: true,
-          ),
-        );
-      } else {
-        context.go("/home");
-      }
-    }
-  }
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: SizedBox(
-          width: 300,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: epostaYonetici,
-                decoration: InputDecoration(
-                  hintText: "E-Posta",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  prefixIcon: Icon(Icons.email),
+      backgroundColor: const Color.fromARGB(255, 233, 116, 155),
+      body: SizedBox.expand(
+        // width: double.infinity,
+        child: Column(
+          children: [
+            // Logo bölümü
+            Expanded(
+              child: Container(
+                width: 150,
+                height: 150,
+                child: Image.asset(
+                  'assets/langugaes/logo.png',
+                  fit: BoxFit.contain,
                 ),
               ),
-              SizedBox(height: 16),
-              TextFormField(
-                obscureText: true,
-                controller: sifreYonetici,
-                decoration: InputDecoration(
-                  hintText: "Sifre",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  prefixIcon: Icon(Icons.lock),
+            ),
+
+            // Yükleniyor yazısı
+             CircularProgressIndicator(),
+
+            InkWell(
+              onTap: () => context.go("/home"),
+              child: SizedBox(
+                width: 300,
+                child: DotLottieLoader.fromAsset(
+                  "assets/motions/animasyon.lottie",
+                  frameBuilder: (BuildContext ctx, DotLottie? dotlottie) {
+                    if (dotlottie != null) {
+                      return Lottie.memory(dotlottie.animations.values.single);
+                    } else {
+                      return Container();
+                    }
+                  },
                 ),
               ),
-              SizedBox(height: 24),
-              ElevatedButton(
-                onPressed: girisYap,
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                child: const Text("Giris Yap", style: TextStyle(fontSize: 16)),
-              ),
-              SizedBox(height: 12),
-              ElevatedButton(
-                onPressed: () {
-                  context.pushReplacement("/register");
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                child: const Text("Kayit Ol", style: TextStyle(fontSize: 16)),
-              ),
-              SizedBox(height: 24),
-              OutlinedButton.icon(
-                onPressed: () {},
-                style: OutlinedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                icon: Image.asset('assets/icons/google_icon.png', height: 24),
-                label: Text("Google ile Giris Yap",
-                    style: TextStyle(fontSize: 16)),
-              ),
-              SizedBox(height: 12),
-              OutlinedButton.icon(
-                onPressed: () {},
-                style: OutlinedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                icon: Image.asset('assets/icons/apple_icon.png', height: 24),
-                label: Text("Apple ID ile Giris Yap",
-                    style: TextStyle(fontSize: 16)),
-              ),
-            ],
-          ),
+            ),
+            // biraz boslok lazim
+            SizedBox(height: 20),
+          ],
         ),
       ),
     );
